e78dede4b0127fb64a0ac3f73801157f8882b931
Tatu Arvela <tatu.arvela@gmail.com>
2016-10-26 09:37:35 +0300

    Added Spring Boot

---
+++ src/main/java/fi/example/PalautekyselyApplication.java
@@ -0,0 +1,12 @@
+package fi.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalautekyselyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalautekyselyApplication.class, args);
+	}
+}
