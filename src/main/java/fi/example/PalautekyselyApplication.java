36f8d1dd2078d5aca949eed9a60adb386b06fb3d
Miikka K <MKA@Miikkas-MacBook-Air-2.local>
2016-10-29 23:09:01 +0300

    Added H2 DB integration, security config, basic form, configured REST controllers to work. Validation is not working ok. Giving http error 500, instead of 400, missing handling

--- src/main/java/fi/example/PalautekyselyApplication.java
+++ src/main/java/fi/example/PalautekyselyApplication.java
@@ -1,12 +1,50 @@
 package fi.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import fi.example.entity.Kysymys;
+import fi.example.entity.KysymysCRUDRepo;
+import fi.example.entity.Vastaus;
+import fi.example.entity.VastausCRUDRepo;
 
 @SpringBootApplication
 public class PalautekyselyApplication {
 
+
+
 	public static void main(String[] args) {
-		SpringApplication.run(PalautekyselyApplication.class, args);
+		SpringApplication.run(PalautekyselyApplication.class);
 	}
-}
+
+	@Bean
+	public CommandLineRunner demo(KysymysCRUDRepo repository, VastausCRUDRepo vasrepo) {
+		return (args) -> {
+			// save a couple of customers
+			
+			vasrepo.save(new Vastaus("Vastaus___1"));
+			vasrepo.save(new Vastaus("Vastaus___2"));
+			
+			Vastaus vastaus = vasrepo.findOne(1L);
+			Kysymys kysymys = new Kysymys("Toimiiko?");
+			Kysymys kysymys2 = new Kysymys("Uuusi Kysymys?");
+			kysymys.getVastauslista().add(vastaus);
+			vastaus = vasrepo.findOne(2L);
+			kysymys2.getVastauslista().add(vastaus);
+			repository.save(kysymys2);
+			kysymys.getVastauslista().add(vastaus);
+			repository.save(kysymys);
+
+
+			repository.save(kysymys);
+			
+
+			
+		
+		};
+	
+		}
+	}
