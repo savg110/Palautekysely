3d343619cff4c4e0b3fe8e5cd1ca3cffa30a9111
Miikka K <uki15@hotmail.com>
2016-11-30 18:47:39 +0200

    added @crossorigin annotation for cross origin requests

--- src/main/java/fi/example/controller/FormController.java
+++ src/main/java/fi/example/controller/FormController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,7 +29,7 @@ import fi.example.entity.VastausCRUDRepo;
 
 
 
-
+@CrossOrigin
 @RestController
 public class FormController {
 	@Autowired
