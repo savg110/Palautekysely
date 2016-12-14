b18c29acba66dde4a0f15e751a2e8e512c2f7b00
Guichly Hessen <Guichly Hessen>
2016-12-14 11:21:16 +0200

    Clean

--- src/main/java/fi/example/controller/FormController.java
+++ src/main/java/fi/example/controller/FormController.java
@@ -23,8 +23,7 @@ import fi.example.entity.VastausCRUDRepo;
 @CrossOrigin
 @Secured("ROLE_ADMIN")
 @RestController
-@RequestMapping (value= "/")
-
+@RequestMapping(value = "/")
 public class FormController {
 	@Autowired
 	KysymysCRUDRepo kysymysrepo;
@@ -32,87 +31,84 @@ public class FormController {
 	VastausCRUDRepo vastausrepo;
 	@Autowired
 	KyselyCRUDRepo kyselyrepo;
-	
-	@GetMapping ("kysymyslista")
+
+	@GetMapping("kysymyslista")
 	public List<Kysymys> haeKysymyksetJSON() {
 		System.out.println(kysymysrepo.findAll());
-		 return (List<Kysymys>) kysymysrepo.findAll();
+		return (List<Kysymys>) kysymysrepo.findAll();
 	}
-	
-	@GetMapping ("kyselylista")
+
+	@GetMapping("kyselylista")
 	public List<Kysely> haeKyselytJSON() {
 		System.out.println(kyselyrepo.findAll());
-		 return (List<Kysely>) kyselyrepo.findAll();
+		return (List<Kysely>) kyselyrepo.findAll();
 	}
-	
-	@GetMapping ("kysymys/{id}")
-	public Kysymys kysymys( @PathVariable long id) {	
-		Kysymys kys =  kysymysrepo.findOne(id);
+
+	@GetMapping("kysymys/{id}")
+	public Kysymys kysymys(@PathVariable long id) {
+		Kysymys kys = kysymysrepo.findOne(id);
 		List<Vastaus> lista = new ArrayList<Vastaus>();
 		kys.setVastauslista(lista);
 		return kys;
 	}
-	
-	@GetMapping ("kysely/{id}")
-	public Kysely Kysely( @PathVariable long id) {	
-		Kysely kysely =  kyselyrepo.findOne(id);
-		List<Kysymys> kysymyslista=kysely.getKysymyslista();
-		List<Vastaus> tyhjalista= new ArrayList<Vastaus>();
-		for (Kysymys kysymys : kysymyslista){
+
+	@GetMapping("kysely/{id}")
+	public Kysely Kysely(@PathVariable long id) {
+		Kysely kysely = kyselyrepo.findOne(id);
+		List<Kysymys> kysymyslista = kysely.getKysymyslista();
+		List<Vastaus> tyhjalista = new ArrayList<Vastaus>();
+		for (Kysymys kysymys : kysymyslista) {
 			kysymys.setVastauslista(tyhjalista);
 		}
 		kysely.setKysymyslista(kysymyslista);
 		return kysely;
 	}
-	
+
 	@PostMapping("lisaakysymys")
-	public String lisaakysymys( @RequestBody List<Kysymys> kysymys ) {
+	public String lisaakysymys(@RequestBody List<Kysymys> kysymys) {
 		Kysely kysely = kyselyrepo.findOne(kysymys.get(0).getId());
 		kysely.getKysymyslista().add(kysymys.get(1));
 		kysymysrepo.save(kysymys.get(1));
-						
+
 		return "200";
 	}
-	
-	@PostMapping(value={"tallenna", "lisaavastaus"})
-	public String tallenna( @RequestBody List<Vastaus> vastauslista) { 
-		System.out.println("vastausta tuli: "+vastauslista);
-		for (Vastaus vastaus : vastauslista){
+
+	@PostMapping(value = { "tallenna", "lisaavastaus" })
+	public String tallenna(@RequestBody List<Vastaus> vastauslista) {
+		System.out.println("vastausta tuli: " + vastauslista);
+		for (Vastaus vastaus : vastauslista) {
 			Kysymys kys = kysymysrepo.findOne(vastaus.getId());
-			vastaus.setId(0L);			
+			vastaus.setId(0L);
 			kys.getVastauslista().add(vastaus);
 			vastausrepo.save(vastaus);
-		}			
+		}
 		return "200";
 	}
-	
+
 	@PostMapping("lisaakysely")
-	public String lisaakysymys( @RequestBody Kysely kysely ) {
-		kyselyrepo.save(kysely);		
-		return "200";
-	}
-	
-	@PostMapping("poistakysymys")
-	public String poistakysymys( @RequestBody Kysely kysely ) {	
+	public String lisaakysymys(@RequestBody Kysely kysely) {
 		kyselyrepo.save(kysely);
 		return "200";
 	}
-	
+
+	@PostMapping("poistakysymys")
+	public String poistakysymys(@RequestBody Kysely kysely) {
+		kyselyrepo.save(kysely);
+		return "200";
+	}
+
 	@PostMapping("poistavastaus")
-	public String poistavastaus( @RequestBody Vastaus id ) {
-		System.out.println("poistettavan id: "+id);
+	public String poistavastaus(@RequestBody Vastaus id) {
+		System.out.println("poistettavan id: " + id);
 		vastausrepo.delete(id);
 		return "200";
 	}
-	
+
 	@PostMapping("poistakysely")
-	public String poistakysely( @RequestBody long id ) {
-		System.out.println("poistettavan id: "+id);
+	public String poistakysely(@RequestBody long id) {
+		System.out.println("poistettavan id: " + id);
 		kyselyrepo.delete(id);
 		return "200";
 	}
-	
-
-	
 
 }
--- src/main/java/fi/example/controller/IndexController.java
+++ src/main/java/fi/example/controller/IndexController.java
@@ -5,41 +5,39 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 public class IndexController {
 
-    @RequestMapping({"/", ""})
-    public String index(HttpServletRequest req){
-    	if(req.isUserInRole("ADMIN")){
-    		
-    		return "/admin/lomakejson";	
-    	}
-    	else if (req.isUserInRole("USER")) {
-    		return "/user/kysely";
-    	}
-    		
-        return "login";
-    }
+	@RequestMapping({ "/", "" })
+	public String index(HttpServletRequest req) {
+		if (req.isUserInRole("ADMIN")) {
 
-    @RequestMapping("/access_denied")
-    public String notAuth(){
-        return "access_denied";
-    }
+			return "/admin/lomakejson";
+		} else if (req.isUserInRole("USER")) {
+			return "/user/kysely";
+		}
 
-    @RequestMapping("login")
-    public String loginForm(){
-        return "login";
-    } 
-    
-    @RequestMapping("/admin/lomakejson")
+		return "login";
+	}
+
+	@RequestMapping("/access_denied")
+	public String notAuth() {
+		return "access_denied";
+	}
+
+	@RequestMapping("login")
+	public String loginForm() {
+		return "login";
+	}
+
+	@RequestMapping("/admin/lomakejson")
 	public String adminIndex() {
 		return "/admin/lomakejson";
 	}
-    
-    @RequestMapping("/user/kysely")
+
+	@RequestMapping("/user/kysely")
 	public String userIndex() {
 		return "/user/kysely";
 	}
-    
+
 }
