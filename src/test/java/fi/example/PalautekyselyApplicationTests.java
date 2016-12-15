5918224d5e6816d5179ad011594df7043a40b71e
Tatu Arvela <tatu.arvela@gmail.com>
2016-12-15 14:45:15 +0200

    Add tests

--- src/test/java/fi/example/PalautekyselyApplicationTests.java
+++ src/test/java/fi/example/PalautekyselyApplicationTests.java
@@ -1,17 +1,90 @@
-/*package fi.example;
+package fi.example;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import fi.example.entity.Kysely;
+import fi.example.entity.KyselyCRUDRepo;
+import fi.example.entity.Kysymys;
+import fi.example.entity.KysymysCRUDRepo;
+import fi.example.entity.Vastaus;
+import fi.example.entity.VastausCRUDRepo;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class PalautekyselyApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PalautekyselyApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    private VastausCRUDRepo vastausrepo;
+	
+    @Autowired
+    public void setVastausrepo(VastausCRUDRepo vastausrepo) {
+        this.vastausrepo = vastausrepo;
+    }
 
+    private KysymysCRUDRepo kysymysrepo;
+
+    @Autowired
+    public void setKysymysrepo(KysymysCRUDRepo kysymysrepo) {
+        this.kysymysrepo = kysymysrepo;
+    }
+    
+	private KyselyCRUDRepo kyselyrepo;
+
+    @Autowired
+    public void setKyselyrepo(KyselyCRUDRepo kyselyrepo) {
+        this.kyselyrepo = kyselyrepo;
+    }
+
+    private Vastaus vastaus;
+    private Kysymys kysymys;
+    private Kysely kysely;
+    
+	@Test
+    public void lisaaVastaus() {
+    	vastaus = new Vastaus("testivastaus");
+
+    	assertNull(vastaus.getId());
+    	vastausrepo.save(vastaus);
+    	assertNotNull(vastaus.getId());
+    }
+    
+    @Test
+    public void lisaaKysymys() {
+    	List<Vastaus> vastauslista = new ArrayList<>();
+    	vastauslista.add(vastaus);
+    	kysymys = new Kysymys("testikysymys", vastauslista);
+
+    	assertNull(kysymys.getId());
+    	kysymysrepo.save(kysymys);
+    	assertNotNull(kysymys.getId());
+    }
+    
+    @Test
+    public void lisaaKysely() {
+    	List<Kysymys> kysymyslista = new ArrayList<>();
+    	kysymyslista.add(kysymys);
+    	kysely = new Kysely("testikysely", "testikyselyn info", kysymyslista);
+
+    	assertNull(kysely.getId());
+    	kyselyrepo.save(kysely);
+    	assertNotNull(kysely.getId());
+    }
 }
-*/
